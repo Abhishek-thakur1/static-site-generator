8c2c10f774b8f605e8bd82181a866e3b72575b60
Abhishek-thakur1 <abhishekcool1804@gmail.com>
2022-05-04 23:56:37 +0530

    completed homepage, modified index.js and added some styling to look cool

--- src/config.js
+++ src/config.js
@@ -4,8 +4,9 @@ const config = {
     blogName: "MY BLOG",
     blogDescription: "Sharing what I learn after attending 3 years of classes at my college",
     authorName: "Abhishek Thakur",
-    authorDescription: "A 3rd year student, lost in Javascript environment, currently learning Blockchain",
+    authorDescription: "a 3rd year student, lost in Javascript environment, currently learning Blockchain",
     authorGithub: "https://github.com/Abhishek-thakur1",
+    authorWebsite: 'https://linkedin.com/in/abhishek-thakur-785201192',
 
     dev: {
         postsdir: './content',
--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ const fs = require('fs');
 
 const config = require('./config')
 const postMethods = require('./posts')
+const addHomePage = require('./homepage')
 
-const posts = fs.readdirSync(config.dev.postsdir).map(post => post.slice(0, -3)).map(post => postMethods.createPost(post))
+const posts = fs.readdirSync(config.dev.postsdir).map(post => post.slice(0, -3)).map(post => postMethods.createPost(post)).sort(function (a, b) {
+    return b.attributes.date - a.attributes.date
+})
 
 
-// check if the file exist, if not then create a new one
+// check if the file exist, if not then create a new one....
 if (!fs.existsSync(config.dev.outdir)) fs.mkdirSync(config.dev.outdir)
 
 postMethods.createPosts(posts);
+addHomePage(posts)
 
 console.log('\nHi, This is STATIC SITE GENERATOR........... \n')
